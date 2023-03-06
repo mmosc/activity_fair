af6b3caeb03bf5f3a6ad92dbe05a387348e29cc0
mmosc <ma.moscati@gmail.com>
2023-03-06 17:08:49 +0100

    add: model_utils.py

---
+++ recbole/model/model_utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def negsamp_vectorized_bsearch_preverif(pos_inds, n_items, n_samp=32):
+    """ Pre-verified with binary search
+    `pos_inds` is assumed to be ordered
+    reference: https://tech.hbc.com/2018-03-23-negative-sampling-in-numpy.html
+    Starting index should be 1 due to RecBole's shitty [PAD]
+    """
+
+    raw_samp = np.random.randint(1, n_items - len(pos_inds), size=n_samp)
+    pos_inds_adj = pos_inds - np.arange(len(pos_inds))
+    neg_inds = raw_samp + np.searchsorted(pos_inds_adj, raw_samp, side='right')
+    return neg_inds
